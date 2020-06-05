b8eec6d81a0d06bd28ebbcdcd1371c7e7e6bc0d7
Nizrum <62435919+Nizrum@users.noreply.github.com>
2020-06-05 18:24:11 +0400

    Добавлена анимация меню
    Добавлена анимация меню

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const menuBtn = document.querySelector('.m-menu');
 const menu = document.querySelector('.menu-sidebar');
+const lines = document.querySelectorAll('.line');
 
 menuBtn.addEventListener('click', function () {
     menu.classList.toggle('active');
+    menuBtn.classList.toggle('active');
 });
