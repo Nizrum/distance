863d8710028ffb5a36e23165c5544fa100f9d521
Nizrum <62435919+Nizrum@users.noreply.github.com>
2020-06-06 16:38:18 +0400

    Добавлено всплывающее окно "Поделиться"
    Добавлено всплывающее окно "Поделиться"

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
 const menuBtn = document.querySelector('.m-menu');
 const menu = document.querySelector('.menu-sidebar');
 const lines = document.querySelectorAll('.line');
+const postButtonShare = document.querySelector('.post-button-share');
+const postShareTitle = document.querySelector('.post-share-title');
+const shareTitleClose = document.querySelector('.share-title-close');
 
 menuBtn.addEventListener('click', function () {
     menu.classList.toggle('active');
     menuBtn.classList.toggle('active');
+});
+
+shareTitleClose.addEventListener('click', function () {
+    postShareTitle.classList.remove('active');
+});
+
+postButtonShare.addEventListener('click', function () {
+    postShareTitle.classList.toggle('active');
 });
